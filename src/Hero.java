40f801f5c7ee4203fca8d6cba353835ee17e8829
sylwia <sylwiadusza280@gmail.com>
2022-11-24 21:12:12 +0100

    tworzenie_klas

--- src/Hero.java
+++ src/Hero.java
@@ -1,16 +1,72 @@
 public class Hero {
-        String name;
-        String abilities;
-        int lvl;
-        double damage;
-        boolean fame;
-        //elementy, które użytkownik musi wpisać tworzać nowego bohatera
-        Hero(String name, String abilities, int lvl, double damage, boolean fame) {
-            this.name = name;   //podpisywnie
-            this.abilities = abilities;
-            this.lvl = lvl;
-            this.damage = damage;
-            this.fame = fame;
-        }
+    private String name;
+    private String abilities;
+    private int lvl;
+    private double damage;
+    private boolean fame;
 
+    //elementy, które użytkownik musi wpisać tworzać nowego bohatera
+
+    public Hero(String name, String abilities, int lvl, double damage, boolean fame) {
+        this.name = name;
+        this.abilities = abilities;
+        this.lvl = lvl;
+        this.damage = damage;
+        this.fame = fame;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(String abilities) {
+        this.abilities = abilities;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    public boolean getFame() {
+        return fame;
+    }
+
+    public void setFame(boolean fame) {
+        this.fame = fame;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", abilities='" + abilities + '\'' +
+                ", lvl=" + lvl +
+                ", damage=" + damage +
+                ", fame=" + fame +
+                '}';
+    }
+
+    public double damagePerSecond () {
+        return damage*5;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
        Hero hero1 = new Hero("Janek", "mag",2, 22.5,true);
-        System.out.println();
+        System.out.println(hero1);
+        System.out.println("Podaj imię");
+        String s = scanner.next();
+        hero1.setName(s);
+        System.out.println(hero1.getName());
+        System.out.println(hero1.getAbilities());
+        System.out.println(hero1.getLvl());
+        System.out.println(hero1.getDamage());
+        System.out.println(hero1.getFame());
+        System.out.println(hero1.getName());
+        hero1.setAbilities("paladyn");
+        System.out.println(hero1.getAbilities());
+        hero1.setDamage(30.1);
+        System.out.println(hero1.getDamage());
+        hero1.setFame(false);
+        System.out.println(hero1.getFame());
+        System.out.println(hero1);
+        System.out.println(hero1.damagePerSecond());
     }
+
 }
